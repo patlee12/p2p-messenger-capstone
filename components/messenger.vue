1ea4fee6c80577fca2ac3cbf5095b01cff1bf9c2
patlee12 <plee1@umassd.edu>
2022-09-14 00:56:27 -0400

    more html

--- components/messenger.vue
+++ components/messenger.vue
@@ -6,8 +6,13 @@ export default defineComponent({
 
   setup() {
     const windowHeight=window.innerHeight*.7
+    const rules= [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ]
   return{
     windowHeight,
+    rules,
   }},
 });
 </script>
@@ -20,7 +25,24 @@ export default defineComponent({
     <v-card-title>
    Configuration
     </v-card-title>
-    <v-card-text>Hello</v-card-text>
+    <v-card-actions>
+       <v-text-field
+      label="Peer ID"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-text-field
+      label="Your Name"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-text-field
+      label="Connect URL"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-btn>Connect</v-btn>
+    </v-card-actions>
   </v-card>
   </v-col>
    <v-col cols="6">
@@ -34,3 +56,8 @@ export default defineComponent({
 </div>
 
 </template>
+<style scoped>
+.v-text-field{
+  padding: 10px;
+}
+</style>
